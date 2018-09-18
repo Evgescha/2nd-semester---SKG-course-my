711cf9d65542fc489d4acded7e27efa6917af001
Evgescha <Yauheni.maslau@yandex.by>
2018-09-18 20:56:09 +0300

    first commit

---
+++ Мой курсач/SKG KurRab/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace SKG_KurRab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(568, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(516, 29);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 42);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(522, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Размер";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(520, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Скорость перемещения";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(522, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Скорость вращения";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(516, 90);
+            this.trackBar2.Maximum = 19;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 42);
+            this.trackBar2.TabIndex = 6;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(516, 151);
+            this.trackBar3.Minimum = 1;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 42);
+            this.trackBar3.TabIndex = 7;
+            this.trackBar3.Value = 1;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(568, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Сброс";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(568, 418);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(516, 212);
+            this.trackBar4.Minimum = 1;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(104, 42);
+            this.trackBar4.TabIndex = 11;
+            this.trackBar4.Value = 1;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(523, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Период";
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(516, 269);
+            this.trackBar5.Minimum = 1;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(104, 42);
+            this.trackBar5.TabIndex = 13;
+            this.trackBar5.Value = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(523, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Время остановки";
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 10;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(662, 453);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(670, 480);
+            this.MinimumSize = new System.Drawing.Size(670, 480);
+            this.Name = "Form1";
+            this.Text = "СКГ. Курсовая работа";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Мой курсач/SKG KurRab/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace SKG_KurRab
+{
+    public partial class Form1 : Form    {
+        //маштаб
+        int mast = 10;
+        //chastot
+        float T;     float TT;
+        //направление движения
+        int dir = 1;
+        //приращение по х
+        float dx = 1;     float dy = 1;
+        //скорость движения и время остановки
+        int TransformSpeed, dT;
+        //скорость вращения
+        float RotateSpeed;
+        //угол поворота
+        float Angle = 0;
+        //координаты центра изображения
+        float xc;    double yc;
+        public Form1() {  InitializeComponent();  }
+        private void Form1_Load(object sender, EventArgs e)  {          
+             xc = Convert.ToInt32(pictureBox1.Width / 2);
+            yc = Convert.ToInt32(pictureBox1.Height / 2);
+            TransformSpeed = 20-trackBar2.Minimum;
+            RotateSpeed = trackBar3.Minimum;
+            T = trackBar4.Minimum;
+            TT = 0;
+            ris();  }
+        private int x1()        {
+            return Convert.ToInt32( xc +((xc - 1.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc - 0.5f * mast))*Math.Sin(Angle));         
+        }
+        private int y1()        {
+            return Convert.ToInt32(yc + ((xc - 1.5f * mast) - xc) * Math.Sin(Angle) + ((yc - 0.5f * mast) - yc) * Math.Cos(Angle));
+        }
+        private int x2()        {
+            return Convert.ToInt32(xc +((xc - 0.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc-0.5f*mast))*Math.Sin(Angle));
+        }
+        private int y2()        {
+            return Convert.ToInt32(yc + ((xc - 0.5f * mast) - xc) * Math.Sin(Angle) + ((yc - 0.5f * mast) - yc) * Math.Cos(Angle));
+       }
+        private int x3()        {
+            return Convert.ToInt32(xc +((xc - 0.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc - 1.5f * mast))*Math.Sin(Angle));
+         }
+        private int y3()        {
+            return  Convert.ToInt32(yc + ((xc - 0.5f * mast) - xc) * Math.Sin(Angle) + ((yc - 1.5f * mast) - yc) * Math.Cos(Angle));
+        }
+        private int x4()        {
+            return Convert.ToInt32(xc +(xc-xc)*Math.Cos(Angle)+(yc-(yc - 0.5f * mast))*Math.Sin(Angle));
+         }
+        private int y4()        {
+            return Convert.ToInt32(yc + (xc - xc) * Math.Sin(Angle) + ((yc - 0.5f * mast) - yc) * Math.Cos(Angle));
+        }
+        private int x5()        {
+            return Convert.ToInt32(xc +((xc + 0.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc - 1.5f * mast))*Math.Sin(Angle));
+        }
+        private int y5()        {
+            return Convert.ToInt32(yc + ((xc + 0.5f * mast) - xc) * Math.Sin(Angle) + ((yc - 1.5f * mast) - yc) * Math.Cos(Angle));
+        }
+        private int x6()        {
+            return Convert.ToInt32(xc +((xc + 0.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc - 0.5f * mast))*Math.Sin(Angle));
+        }
+        private int y6()        {
+            return Convert.ToInt32(yc + ((xc + 0.5f * mast) - xc) * Math.Sin(Angle) + ((yc - 0.5f * mast) - yc) * Math.Cos(Angle));
+       }
+        private int x7()        {
+            return Convert.ToInt32(xc +((xc + 1.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc - 0.5f * mast))*Math.Sin(Angle));
+        }
+        private int y7()        {
+            return Convert.ToInt32(yc + ((xc + 1.5f * mast) - xc) * Math.Sin(Angle) + ((yc - 0.5f * mast) - yc) * Math.Cos(Angle));
+        }
+        private int x8()        {
+            return Convert.ToInt32(xc +((xc + 0.5f * mast)-xc)*Math.Cos(Angle)+(yc-yc)*Math.Sin(Angle));
+        }
+        private int y8()        {
+            return Convert.ToInt32(yc + ((xc + 0.5f * mast) - xc) * Math.Sin(Angle) + (yc - yc) * Math.Cos(Angle));
+        }
+        private int x9()        {
+            return Convert.ToInt32(xc +((xc + 1.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc + 0.5f * mast))*Math.Sin(Angle));
+        }
+        private int y9()        {
+            return Convert.ToInt32(yc + ((xc + 1.5f * mast) - xc) * Math.Sin(Angle) + ((yc + 0.5f * mast) - yc) * Math.Cos(Angle));
+         }
+        private int x10()        {
+            return Convert.ToInt32(xc +((xc + 0.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc + 0.5f * mast))*Math.Sin(Angle));
+        }
+        private int y10()        {
+            return Convert.ToInt32(yc + ((xc + 0.5f * mast) - xc) * Math.Sin(Angle) + ((yc + 0.5f * mast) - yc) * Math.Cos(Angle));
+        }
+        private int x11()        {
+            return Convert.ToInt32(xc +((xc + 0.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc + 1.5f * mast))*Math.Sin(Angle));
+       }
+        private int y11()        {
+            return Convert.ToInt32(yc + ((xc + 0.5f * mast) - xc) * Math.Sin(Angle) + ((yc + 1.5f * mast) - yc) * Math.Cos(Angle));
+        }
+        private int x12()        {
+            return Convert.ToInt32(xc + ((xc) - xc) * Math.Cos(Angle) + (yc - (yc + 0.5f * mast)) * Math.Sin(Angle));
+        }
+        private int y12()        {
+            return Convert.ToInt32(yc + ((xc) - xc) * Math.Sin(Angle) + ((yc + 0.5f * mast) - yc) * Math.Cos(Angle));
+        }
+        private int x13()        {
+            return Convert.ToInt32(xc +((xc - 0.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc + 1.5f * mast))*Math.Sin(Angle));
+       }
+        private int y13()        {
+            return Convert.ToInt32(yc + ((xc - 0.5f * mast) - xc) * Math.Sin(Angle) + ((yc + 1.5f * mast) - yc) * Math.Cos(Angle));
+        }
+        private int x14()        {
+            return Convert.ToInt32(xc +((xc - 0.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc + 0.5f * mast))*Math.Sin(Angle));
+       }
+        private int y14()        {
+            return Convert.ToInt32(yc + ((xc - 0.5f * mast) - xc) * Math.Sin(Angle) + ((yc + 0.5f * mast) - yc) * Math.Cos(Angle));
+        }
+        private int x15()        {
+            return Convert.ToInt32(xc +((xc - 1.5f * mast)-xc)*Math.Cos(Angle)+(yc-(yc + 0.5f * mast))*Math.Sin(Angle));
+       }
+        private int y15()        {
+            return Convert.ToInt32(yc + ((xc - 1.5f * mast) - xc) * Math.Sin(Angle) + ((yc + 0.5f * mast) - yc) * Math.Cos(Angle));
+        }
+        private int x16()        {
+            return Convert.ToInt32(xc +((xc - 0.5f * mast)-xc)*Math.Cos(Angle)+(yc-yc)*Math.Sin(Angle));
+        }
+        private int y16()        {
+            return Convert.ToInt32(yc + ((xc - 0.5f * mast) - xc) * Math.Sin(Angle) + (yc - yc) * Math.Cos(Angle));
+        }
+        //кнопка старт
+        private void button1_Click(object sender, EventArgs e)        {
+            timer1.Start();
+            timer2.Start();
+        }
+        private void ris()        {
+            //присваиваю g рисование графиков
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //Graphics g = pictureBox1.CreateGraphics();
+            Graphics g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+            //создаю ручку
+            Pen pen = new Pen(Color.Green);
+            //сброс
+            g.Clear(SystemColors.Control);
+            //точки фигуры для полигона
+            Point point1 = new Point(x1(), y1());
+            Point point2 = new Point(x2(), y2());
+            Point point3 = new Point(x3(), y3());
+            Point point4 = new Point(x4(), y4());
+            Point point5 = new Point(x5(), y5());
+            Point point6 = new Point(x6(), y6());
+            Point point7 = new Point(x7(), y7());
+            Point point8 = new Point(x8(), y8());
+            Point point9 = new Point(x9(), y9());
+            Point point10 = new Point(x10(), y10());
+            Point point11 = new Point(x11(), y11());
+            Point point12 = new Point(x12(), y12());
+            Point point13 = new Point(x13(), y13());
+            Point point14 = new Point(x14(), y14());
+            Point point15 = new Point(x15(), y15());
+            Point point16 = new Point(x16(), y16());
+            //массив из точек для полигона фигуры
+            Point[] curvePoints =    {
+                 point1,
+                 point2,
+                 point3,
+                 point4,
+                 point5,
+                 point6,
+                 point7,
+                 point8,
+                 point9,
+                 point10,
+                 point11,
+                 point12,
+                 point13,
+                 point14,
+                 point15,
+                 point16,             }; 
+            //массив фигуры для заднего фона
+            Point[] TrPoints =           {
+                 point3,
+                 point7,
+                 point11,
+                 point15            };
+            //рисование заднего поле закрашенного
+            Brush br1 = new SolidBrush(Color.FromArgb(0, colorDec(), colorInc()));
+            Brush br2 = new SolidBrush(Color.FromArgb(colorInc(), colorDec(), 0));
+            g.FillPolygon(br1, TrPoints);
+            //рисование закрашенной фигуры
+            g.FillPolygon(br2, curvePoints);        }
+        //размер фигуры
+        private void trackBar1_Scroll(object sender, EventArgs e)        {
+            mast = trackBar1.Value;        
+            ris();        }
+        private void trackBar2_Scroll(object sender, EventArgs e)        {
+            TransformSpeed = 20 - Convert.ToInt32(trackBar2.Value);
+            ris();        }
+        private void trackBar3_Scroll(object sender, EventArgs e)        {
+            RotateSpeed = trackBar3.Value;
+            ris();        }
+        private void timer1_Tick(object sender, EventArgs e)        {
+              if (x1() > pictureBox1.Width ||
+                x2() > pictureBox1.Width ||
+                x3() > pictureBox1.Width ||
+                x4() > pictureBox1.Width ||
+                x5() > pictureBox1.Width ||
+                x6() > pictureBox1.Width ||
+                x7() > pictureBox1.Width ||
+                x8() > pictureBox1.Width ||
+                x9() > pictureBox1.Width ||
+                x10() > pictureBox1.Width ||
+                x11() > pictureBox1.Width ||
+                x12() > pictureBox1.Width ||
+                x13() > pictureBox1.Width ||
+                x14() > pictureBox1.Width ||
+                x15() > pictureBox1.Width ||
+                x16() > pictureBox1.Width)            {
+                xc = xc - 2;
+                dx = -dx;
+                dir = -dir;
+                timer3.Start();
+            }
+            if( y1() > pictureBox1.Height ||
+                y2() > pictureBox1.Height ||
+                y3() > pictureBox1.Height ||
+                y4() > pictureBox1.Height ||
+                y5() > pictureBox1.Height ||
+                y6() > pictureBox1.Height ||
+                y7() > pictureBox1.Height ||
+                y8() > pictureBox1.Height ||
+                y9() > pictureBox1.Height ||
+                y10() > pictureBox1.Height ||
+                y11() > pictureBox1.Height ||
+                y12() > pictureBox1.Height ||
+                y13() > pictureBox1.Height ||
+                y14() > pictureBox1.Height ||
+                y15() > pictureBox1.Height ||
+                y16() > pictureBox1.Height)            {
+                yc = yc - 2;
+                dy = -dy;           }
+            if (            x1() < 0 ||
+                            x2() < 0 ||
+                            x3() < 0 ||
+                            x4() < 0 ||
+                            x5() < 0 ||
+                            x6() < 0 ||
+                            x7() < 0 ||
+                            x8() < 0 ||
+                            x9() < 0 ||
+                            x10() < 0 ||
+                            x11() < 0 ||
+                            x12() < 0 ||
+                            x13() < 0 ||
+                            x14() < 0 ||
+                            x15() < 0 ||
+                            x16() < 0)            {
+                xc = xc + 2;
+                dx = -dx;
+                dir = -dir; timer3.Start();;            }
+            if (            y1() < 0 ||
+                            y2() < 0 ||
+                            y3() < 0 ||
+                            y4() < 0 ||
+                            y5() < 0 ||
+                            y6() < 0 ||
+                            y7() < 0 ||
+                            y8() < 0 ||
+                            y9() < 0 ||
+                            y10() < 0 ||
+                            y11() < 0 ||
+                            y12() < 0 ||
+                            y13() < 0 ||
+                            y14() < 0 ||
+                            y15() < 0 ||
+                            y16() < 0)            {
+                yc = yc + 2;
+                dy = -dy;           }
+            ris();           
+            xc = xc + dx;
+            yc = (pictureBox1.Height / 2 + ((pictureBox1.Height - 3 * mast) / 2) * Math.Sin(21 * (TT/10)))-2;
+            Angle += 0.01f * RotateSpeed * dir;
+            TT += 0.01f * T;        }
+        private void button2_Click(object sender, EventArgs e)        {
+            timer1.Stop();
+            timer2.Stop();
+            xc = Convert.ToInt32(pictureBox1.Width / 2);
+            yc = Convert.ToInt32(pictureBox1.Height / 2);
+            TransformSpeed = 20 - trackBar2.Minimum;
+            trackBar2.Value = trackBar2.Minimum;
+            RotateSpeed = trackBar3.Minimum;
+            trackBar3.Value = trackBar3.Minimum;
+            mast = trackBar1.Minimum;
+            trackBar1.Value = trackBar1.Minimum;
+            trackBar4.Value = trackBar4.Minimum;
+            trackBar5.Value = trackBar5.Minimum;
+            Angle = 0;TT = 0;
+            T = trackBar4.Minimum;
+            ris();        }
+        private void button3_Click(object sender, EventArgs e)        {
+            Application.Exit();        }
+        private void timer2_Tick(object sender, EventArgs e)        {      
+            timer1.Interval = TransformSpeed;       }
+        private void trackBar4_Scroll(object sender, EventArgs e)        {
+            T = trackBar4.Value;        }
+        private byte colorInc()        {
+            try           {
+                double col = 255 * (xc - 1.5 * mast) / (pictureBox1.Width - 1.5 * mast - 1.5 * mast);
+                return Convert.ToByte(col);            }
+            catch { return 0; }        }
+        private byte colorDec()        {
+            try            {
+                double col = -255 * (xc - 1.5 * mast) / (pictureBox1.Width - 1.5 * mast - 1.5 * mast) + 255;
+                return Convert.ToByte(col);            }
+            catch { return 0; }        }
+        private void timer3_Tick(object sender, EventArgs e){if (dT > 100) {
+                timer1.Start(); timer2.Start(); dT = 0; timer3.Stop(); }  else{
+                timer1.Stop(); timer2.Stop(); dT+=11-trackBar5.Value; }}}}
